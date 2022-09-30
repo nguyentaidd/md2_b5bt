be04f3878a97d122765b8909847dd83ce9bfa309
nguyentaidd <nguyenhtai@gmail.com>
2022-09-30 09:55:28 +0700

    first commit

---
+++ src/ra/Circle.java
@@ -0,0 +1,20 @@
+package ra;
+
+public class Circle {
+    private double radius = 1;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return this.radius*this.radius*Math.PI;
+    }
+}
